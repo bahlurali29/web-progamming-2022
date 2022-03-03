5712f1585b4a5e8aebbc6facaa995d2bc6d5e7d8
bahlurali29 <98538549+bahlurali29@users.noreply.github.com>
2022-03-03 10:04:48 +0700

    Add files via upload

---
+++ array_siswa.php
@@ -0,0 +1,341 @@
+online
+Gue pintain di haqi
+20.03
+Gue punya neh jawaban komputer jaringan buat pertemuan besok
+20.11
+Wildan · Status
+Today is Persija Day🐯
+Kirim es
+21.27
+0:03
+22.02
+Nyolong dah gue mah
+22.04
+0:06
+22.44
+0:05
+22.46
+KEMARIN
+Mana bagi pw
+13.11
+Basis data udeh blom?
+13.11
+Blman cok
+13.11
+Gue ge nunggu hakim
+13.12
+Mtk lu udh blm
+13.12
+Woi statistik probilitas udh blm
+14.11
+Bloman
+14.19
+Gua lagi narik
+14.19
+Ok dahh
+14.32
+Woii mek mana pw:/
+22.00
+Bloman anj demi Allah
+22.01
+Okok ler
+22.28
+HARI INI
+Gue dah punya jawaban kompyter jaringin neh
+07.52
+Pw udeh nih
+08.29
+Mau ga?
+08.29
+Tengah malem gua kerjain
+08.29
+Mana ce alokan gue ngerjain jaringan jam 2
+08.30
+Wildan
+Mau ga?
+Mane
+08.30
+Yaudeh bagi sini jaringan
+08.31
+Yaudhh bagi dlu sini pw
+08.31
+Probilitas udh blm lu
+08.33
+Anda
+Yaudhh bagi dlu sini pw
+<html> 
+ <head> 
+     <meta charset="UTF-8"> 
+     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+     <title>praktikum1</title> 
+     <!-- css bootstrap --> 
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> 
+     <style> 
+     .nilai { 
+         padding: 20px; 
+     } 
+     </style> 
+ </head> 
+ <body style="background-color: bisque;"> 
+     <!-- navbar --> 
+     <nav class="navbar navbar-expand-lg navbar-light bg-warning"> 
+         <div class="container-fluid"> 
+             <a class="navbar-brand" href="#">S… Baca selengkapnya
+08.34
+Anda
+Probilitas udh blm lu
+Bloman
+08.34
+Mana jaringan
+08.34
+Wildan
+<html> 
+ <head> 
+     <meta charset="UTF-8"> 
+     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+     <title>praktikum1</title> 
+     <!-- css bootstrap --> 
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> 
+     <style> 
+     .nilai { 
+         padding: 20px; 
+     } 
+     </style> 
+ </head> 
+ <body style="background-color: bisque;"> 
+     <!-- navbar --> 
+     <nav class="navbar navbar-expand-lg navbar-light bg-warning"> 
+         <div class="container-fluid"> 
+             <a class="navbar-brand" href="#">S
+Ada yang perlu gue edit ga ini
+08.34
+Wildan
+Mana jaringan
+Sabar tos:/
+08.34
+Udeh kelar
+08.34
+Jan kasi ke orang" lu boleh gue cape" mikir:/
+08.35
+khusus nya yaitu, karena ini adalah layer yang memiliki wujud fisik yaitu kabel misalnya atau gelombangatau Hal lainnya Dan bisa berupa hardware hardware (no 1)
+
+terdapat 3 Jeni's physical layer :
+1. kabel tembaga, sinyal berbentuk pulsa listrik (kabel UTP)
+2. kabel fiber optic, sinyal berbentuk pola cahaya
+3. wireless, sinyal berbentuk gelombang radio di udara (sinyal wifi)
+(no 2)
+
+protokol yg diikuti adalah protokol yg ketiga
+bagaimana dunia mau mengikuti aturan tersebut? walaupun layer 1 dan 2 tidak di definisikan, tapi tetap ada yg ngatur karena itulah fungsi protokol, si protokol inilah yg akan mengatur si layer 1 dan 2 
+(no 3)
+
+IEEE 802.3 untuk Ethernet
+IEEE 802.11 untuk Wireless LAN
+IEEE 802.15 untuk Wireless PAN (ada Bluetooth)
+(no 4)
+
+artinya dia co… Baca selengkapnya
+08.35
+Anda
+Jan kasi ke orang" lu boleh gue cape" mikir:/
+Iyee santuy
+08.35
+Wildan
+Iyee santuy
+Okok
+08.36
+Basis data gue blm dpt lagi
+08.36
+Wildan
+<html> 
+ <head> 
+     <meta charset="UTF-8"> 
+     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+     <title>praktikum1</title> 
+     <!-- css bootstrap --> 
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> 
+     <style> 
+     .nilai { 
+         padding: 20px; 
+     } 
+     </style> 
+ </head> 
+ <body style="background-color: bisque;"> 
+     <!-- navbar --> 
+     <nav class="navbar navbar-expand-lg navbar-light bg-warning"> 
+         <div class="container-fluid"> 
+             <a class="navbar-brand" href="#">S
+Coba aje lu cek lagi
+08.36
+Anda
+Basis data gue blm dpt lagi
+Masih lama
+08.36
+Statistik dulu deh
+08.36
+Wildan
+Coba aje lu cek lagi
+Ini gada yabg perlu gue ubah lagi kan
+08.36
+Wildan
+Statistik dulu deh
+Gue probilitas lagi usaha ini
+08.37
+Anda
+Ini gada yabg perlu gue ubah lagi kan
+Kaga tapi kalo lu mau beda Ama gua mah gpp lu ganti CSS nye
+08.38
+Gc up github
+08.38
+Ama kreasi
+08.38
+Wildan
+Gc up github
+Ehh gue lupa cara aploud ny
+08.39
+Wildan
+Ama kreasi
+Kreasi apa maksdnya cok
+08.39
+0:18
+08.40
+0:03
+08.41
+Coba dan lu rekam layar cara buat aploudnya
+08.42
+Gue lupa:/
+08.42
+git init
+08.44
+0:04
+08.44
+git add "namaFile"
+08.44
+Wildan
+git add "namaFile"
+Nama file ny apa cok
+08.45
+0:03
+08.45
+0:03
+08.45
+0:10
+08.45
+Anda
+0:03
+0:05
+08.45
+Wildan
+0:10
+Apa nama file nt
+08.45
+Wildan
+0:05
+Siapp
+08.45
+array_siswa.php
+08.46
+Wildan
+array_siswa.php
+Nama file nye
+08.46
+Coba lu ss vs code nye
+08.46
+Wildan
+Coba lu ss vs code nye
+Sabar ce laptop gue lemot anj:/
+08.47
+<html> 
+ <head> 
+     <meta charset="UTF-8"> 
+     <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+     <title>praktikum1</title> 
+     <!-- css bootstrap --> 
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> 
+     <style> 
+     .nilai { 
+         padding: 20px; 
+     } 
+     </style> 
+ </head> 
+ <body style="background-color: bisque;"> 
+     <!-- navbar --> 
+     <nav class="navbar navbar-expand-lg navbar-light bg-warning"> 
+         <div class="container-fluid"> 
+             <a class="navbar-brand" href="#">SITE</a> 
+             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> 
+             <span class="navbar-toggler-icon"></span> 
+             </button> 
+             <div class="collapse navbar-collapse" id="navbarNav"> 
+                 <ul class="navbar-nav"> 
+                     <li class="nav-item "> 
+                         <a class="nav-link active" aria-current="page" href="#home">Home</a> 
+                     </li> 
+                     <li class="nav-item"> 
+                         <a class="nav-link" href="#about">About</a> 
+                     </li> 
+                     <li class="nav-item"> 
+                         <a class="nav-link" href="#feature">feature</a> 
+                     </li> 
+                     <li class="nav-item"> 
+                         <a class="nav-link" href="#social">social</a> 
+                     </li> 
+                     <li class="nav-item"> 
+                         <a class="nav-link" href="#contact">Contact</a> 
+                     </li> 
+                 </ul> 
+             </div> 
+         </div> 
+     </nav> 
+     <!-- jumbotron --> 
+     <div class="jumbotron text-center"> 
+         <h1 class="display-4">Nilai Hasil Pembelajaran</h1> 
+         <p class="lead">semester1</p> 
+         <hr class="my-4"> 
+         <a class="btn btn-primary btn-lg" href="#nilai" role="button">Lihat Nilai</a> 
+     </div> 
+ <section id="nilai"> 
+ <div class="nilai"> 
+ <!-- syntax php --> 
+ <?php 
+ $ns1 = ['id'=>1,'nim'=>'01101','uts'=>80,'uas'=>84,'tugas'=>78]; 
+ $ns2 = ['id'=>2,'nim'=>'01121','uts'=>70,'uas'=>50,'tugas'=>68]; 
+ $ns3 = ['id'=>3,'nim'=>'01130','uts'=>60,'uas'=>86,'tugas'=>70]; 
+ $ns4 = ['id'=>4,'nim'=>'01134','uts'=>90,'uas'=>91,'tugas'=>82]; 
+  
+ $ar_nilai = [$ns1, $ns2 , $ns3, $ns4]; 
+  
+ ?> 
+ <h3 style="text-align: center;">Daftar Nilai Siswa</h3> 
+ <h4 ><b>Note :</b> silahkan hubungi dosen untuk melakukan perbaikan.</h4> 
+ <table border="1" width="100%"> 
+     <thead> 
+         <tr> 
+             <th>No</th><th>NIM</th><th>UTS</th> 
+             <th>UAS</th><th>Tugas</th><th>Nilai Akhir</th> 
+         </tr> 
+     </thead> 
+     <tbody> 
+         <?php 
+         $nomor = 1; 
+         foreach($ar_nilai as $ns) { 
+             echo '<tr><td>'.$nomor.'</td>'; 
+             echo '<td>'.$ns['nim'].'</td>'; 
+             echo '<td>'.$ns['uts'].'</td>'; 
+             echo '<td>'.$ns['uas'].'</td>'; 
+             echo '<td>'.$ns['tugas'].'</td>'; 
+             $nilai_akhir = ($ns['uts'] + $ns['uas']+$ns['tugas'])/3; 
+             echo '<td>'.number_format($nilai_akhir,2,',','.').'</td>'; 
+             echo '<tr/>'; 
+             $nomor++; 
+         } 
+         ?> 
+ </div> 
+ </tbody> 
+ </section> 
+ </body>
+</html>
